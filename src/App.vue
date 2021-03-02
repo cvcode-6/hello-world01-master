39d01baa2c86212a6fc1b664131e4ff8d1499987
blackbaby <blackbaby@qq.com>
2021-03-02 17:19:41 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+      <p>java0821 全体同学 前程似锦</p>
+      <p>java0821 全体同学 貌美如花</p>
+      <p>祝 java0821 全体同学 拿到自己心仪的offer</p>
+      <p>祝 java0821 全体同学 拿到自己心仪的offer</p>
+   <img src="https://cn.bing.com/images/search?view=detailV2&ccid=647wRlcK&id=72DC4450784E660068F751FA52586769536E222B&thid=OIP.647wRlcKPSI0eIyxxFnyTQHaHa&mediaurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fReb8ef046570a3d2234788cb1c459f24d%3frik%3dKyJuU2lnWFL6UQ%26riu%3dhttp%253a%252f%252fimg.yoyou.com%252fuploadfile%252f2017%252f0727%252f20170727014939523.jpg%26ehk%3d1vP6oOtYMFMbFNf3xcDag9PTSHxMtpeDXFMpA%252fjo7h4%253d%26risl%3d%26pid%3dImgRaw&exph=640&expw=640&q=%e6%90%9e%e7%ac%91%e5%9b%be%e7%89%87%e7%86%8a%e7%8c%ab%e5%a4%b4&simid=608020537737940289&ck=57F71D920D8C84AC74E24F15266B4CC5&selectedIndex=15&qpvt=%e6%90%9e%e7%ac%91%e5%9b%be%e7%89%87%e7%86%8a%e7%8c%ab%e5%a4%b4&FORM=IRPRST"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
